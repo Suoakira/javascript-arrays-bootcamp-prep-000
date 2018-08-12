e46d34329fd5e4854406474931398347e97c7c7d
Steve James <steves0089@gmail.com>
2018-08-12 12:49:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.pop(element);
+  array.unshift(element);
   return;
 }
